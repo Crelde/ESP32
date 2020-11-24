b6a2aced203f9675928bd5e4b644ed78f538bea5
dkchhe <dkchhe@chr-hansen.com>
2020-11-24 13:17:34 +0100

    esp32 set up for me

--- http-receiver/main/config.h
+++ http-receiver/main/config.h
@@ -3,27 +3,24 @@
 
 /* ----- CONFIG FILE ----- */
 
-
 /* Device */
-const char* id_name = "ESP32-GETTER";
+const char *id_name = "ESP32-GETTER";
 
 /* Network Configuration */
-const char* ssid_WiFi = "mySSID";
-const char* pass_WiFi = "myPASS";
+const char *ssid_WiFi = "Crelde&LinoCrib";
+const char *pass_WiFi = "YoloSwag";
 
 /* HTTP Endpoint Configuration */
-const char* address = "192.168.1.101/messages/last";  /* Endpoint address (HTTP), must NOT include 'http://xxx' or 'tcp://xxx', and must include '/sensor_data' for using in I2T Gateway*/
+const char *address = "94.16.114.51/messages/last"; /* Endpoint address (HTTP), must NOT include 'http://xxx' or 'tcp://xxx', and must include '/sensor_data' for using in I2T Gateway*/
 int port = 3002;
 
 /* Enable Relays */
 bool isEnable_Relay_1 = true;
-bool isEnable_Relay_2 = true;	/*			true: Enable  --  false: Disable			*/
-bool isEnable_Relay_3 = true;
-bool isEnable_Relay_4 = true;
-
+bool isEnable_Relay_2 = false; /*			true: Enable  --  false: Disable			*/
+bool isEnable_Relay_3 = false;
+bool isEnable_Relay_4 = false;
 
 /* Interval of time */
-long interval = 15;    /* Time in seconds between */
-
+long interval = 15; /* Time in seconds between */
 
 #endif
--- http-receiver/main/core/ESP32/output/relay/relay.c
+++ http-receiver/main/core/ESP32/output/relay/relay.c
@@ -8,20 +8,20 @@
 #include "driver/gpio.h"
 
 #define NUM_RELAYS 4
-#define RELAY_1 26		// RELAY 1 in GPIO26
-#define RELAY_2 27		// RELAY 2 in GPIO27
-#define RELAY_3 14		// RELAY 3 in GPIO14
-#define RELAY_4 12		// RELAY 4 in GPIO12
+#define RELAY_1 26 // RELAY 1 in GPIO26
+#define RELAY_2 27 // RELAY 2 in GPIO27
+#define RELAY_3 14 // RELAY 3 in GPIO14
+#define RELAY_4 12 // RELAY 4 in GPIO12
 
-const int num[NUM_RELAYS] = { RELAY_1, RELAY_2, RELAY_3,  RELAY_4};
+const int num[NUM_RELAYS] = {RELAY_1, RELAY_2, RELAY_3, RELAY_4};
 
 void init_relay()
 {
-	for (int i=0; i<NUM_RELAYS; i++)
+	for (int i = 0; i < NUM_RELAYS; i++)
 	{
 		gpio_pad_select_gpio(num[i]);
 		gpio_set_direction(num[i], GPIO_MODE_OUTPUT);
-		gpio_set_level(num[i], 0);	// Init as LOW
+		gpio_set_level(num[i], 0); // Init as LOW
 	}
 }
 
